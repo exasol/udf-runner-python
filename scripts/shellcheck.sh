01e7be6aeb1e6aa86de982e65f1934c1350a6a01
Shankar.KV <shankar.vaithiyanadhaswamy@exasol.com>
2026-08-26 12:56:05 +0530

    removed githooks

--- scripts/shellcheck.sh
+++ scripts/shellcheck.sh
@@ -2,7 +2,7 @@
 
 set -u
 
-interesting_paths=("scripts" "emulator" "exaudfclient" "ext" "githooks")
+interesting_paths=("scripts" "emulator" "exaudfclient" "ext")
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 status=0
