488648eac47e9dc4959762276c66ae692beca20c
Shankar.KV <shankar.vaithiyanadhaswamy@exasol.com>
2026-08-31 16:57:19 +0530

    Preserve Conda bin directory in runner PATH

--- exaudfclient/exa_vm_factory.cc
+++ exaudfclient/exa_vm_factory.cc
@@ -1,4 +1,6 @@
 #include <functional>
+#include <cstdlib>
+#include <iostream>
 
 #include "exa_vm_factory.h"
 
@@ -11,6 +13,14 @@ std::function<SWIGVMContainers::SWIGVM*()> create_vm(const std::string& argv_lan
         throw SWIGVMContainers::SWIGVM::exception("unsupported language specified in argv");
     }
 #ifdef ENABLE_PYTHON_VM
+    char *path_var = getenv("PATH");
+    if (path_var != nullptr) {
+        std::string path_var_str = std::string(path_var);
+        path_var_str.insert(0, "/opt/conda/bin:");
+        if (setenv("PATH", path_var_str.c_str(), 1) == -1) {
+            std::cerr << "Unable to prefix PATH env variable with /opt/conda/bin";
+        }
+    }
     return []() { return new SWIGVMContainers::PythonVM(false); };
 #else
     throw SWIGVMContainers::SWIGVM::exception("this exaudfclient has been compiled without Python support");
